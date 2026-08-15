0ff739f9feb7de040d4a225e66350765924970e1
iriswildauer <i-wildauer@t-online.de>
2026-08-15 22:40:39 +0200

    parser

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 // (˶>⩊<˶)
 
 enum TokenType {
+    EOF_TOK,
     UNKNOWN,
     //keywords
     PRINT_TOK,
@@ -28,6 +29,7 @@ struct Parser {const struct Token *token; int n; int pos;} Parser;
 
 static const char *tokenName(enum TokenType k) {
     switch (k) {
+        case EOF_TOK: return "EOF";
         case PRINT_TOK: return "PRINT_TOK";
         case STRING_LITERAL: return "STRING_LITERAL";
         case ADD: return "ADD";
@@ -93,11 +95,12 @@ int lexer(char *output, int len, struct Token **lex, int *numtokens) {
             n++;
         }
         else if (isdigit(output[i])) {
-            token[n].kind = NUMBER;
-            token[n].len = 1;
-            token[n].start = &output[i];
+            int start = i;
+            while (isdigit(output[i])) i++;
+            token[n].kind  = NUMBER;
+            token[n].start = &output[start];
+            token[n].len   = i - start;
             n++;
-            i++;
         }
         else if (output[i] == '*') {
             token[n].kind = MULT;
@@ -115,6 +118,10 @@ int lexer(char *output, int len, struct Token **lex, int *numtokens) {
         }
     }
 
+    token[n].kind = EOF_TOK;
+    token[n].len = 0;
+    token[n].start = &output[len];
+    n++;
     *lex = token;
     *numtokens = n;
 
@@ -167,13 +174,55 @@ int parseFile(const char *file,char **output,int *len) {
 //factor    => NUMBER | STRING
 //factor    => NUMBER | STRING
 
+typedef struct Expr Expr;
+struct Expr {
+    enum {NUM, STRING, OPERATOR} kind;
+    union {
+        int Number;
+        struct {const char *start; int len;} String;
+        struct {enum TokenType operation; Expr *left, *right;} Operator;
+    } as ;
+};
+
+static Expr *newNumber(const int num) {
+    Expr *e = malloc(sizeof *e);
+    if (e == NULL) return NULL;
+
+    e->kind = NUM;
+    e->as.Number = num;
+
+    return e;
+}
+
+static Expr *newString(const char *start, int len) {
+    Expr *e = malloc(sizeof *e);
+    if (e == NULL) return NULL;
+
+    e->kind = STRING;
+    e->as.String.start = start;
+    e->as.String.len = len;
+
+    return e;
+}
+
+static Expr *newOp(enum TokenType op, Expr *left, Expr *right) {
+    Expr *e = malloc(sizeof *e);
+    if (e == NULL) return NULL;
+
+    e->kind = OPERATOR;
+    e->as.Operator.left = left;
+    e->as.Operator.right = right;
+    e->as.Operator.operation = op;
+
+    return e;
+}
+
 struct Token peek(struct Parser *parser) {
     return parser->token[parser->pos];
 }
 
 struct Token advance(struct Parser *parser) {
-    parser->pos++;
-    return parser->token[parser->pos];
+    return parser->token[parser->pos++];
 }
 
 int match(enum TokenType type, struct Parser *parser) {
@@ -191,23 +240,114 @@ int expect(enum TokenType type, struct Parser *parser) {
     return 0;
 };
 
+
+static Expr *parseExpr(struct Parser *parser);
+
+static Expr *parseStatement(struct Parser *parser) {
+    if (match(PRINT_TOK, parser)) {
+        return parseExpr(parser);
+    }
+
+    struct Token t = peek(parser);
+    fprintf(stderr, "Syntaxfehler: %s ('%.*s')\n",
+            tokenName(t.kind), t.len, t.start);
+    return NULL;
+}
+
+static Expr *parseFactor(struct Parser *parser) {
+    struct Token t = peek(parser);
+
+    if (t.kind == NUMBER) {
+        advance(parser);
+        return newNumber((int)strtol(t.start, NULL, 10));
+    }
+
+    if (t.kind == STRING_LITERAL) {
+        advance(parser);
+        return newString(t.start, t.len);
+    }
+
+    fprintf(stderr, "Syntaxfehler: %s ('%.*s')\n",
+            tokenName(t.kind), t.len, t.start);
+    return NULL;
+}
+
+static Expr *parseTerm(struct Parser *parser) {
+    Expr *left = parseFactor(parser);
+    if (left == NULL) return NULL;
+
+    while (match(MULT, parser)) {
+        Expr *right = parseFactor(parser);
+        if (right == NULL) return NULL;
+        left = newOp(MULT, left, right);
+    }
+    return left;
+}
+
+static Expr *parseExpr(struct Parser *parser) {
+    Expr *left = parseTerm(parser);
+    if (left == NULL) return NULL;
+
+    while (match(ADD, parser)) {
+        Expr *right = parseTerm(parser);
+        if (right == NULL) return NULL;
+        left = newOp(ADD, left, right);
+    }
+    return left;
+}
+
+static void printExpr(const Expr *e, int indent) {
+    if (e == NULL) return;
+    for (int i = 0; i < indent; i++) printf("  ");
+
+    switch (e->kind) {
+        case NUM:
+            printf("Number(%d)\n", e->as.Number);
+            break;
+        case STRING:
+            printf("String(\"%.*s\")\n", e->as.String.len, e->as.String.start);
+            break;
+        case OPERATOR:
+            printf("Operator(%s)\n", tokenName(e->as.Operator.operation));
+            printExpr(e->as.Operator.left,  indent + 1);
+            printExpr(e->as.Operator.right, indent + 1);
+            break;
+    }
+}
+
+static void freeExpr(Expr *e) {
+    if (e == NULL) return;
+    if (e->kind == OPERATOR) {
+        freeExpr(e->as.Operator.left);
+        freeExpr(e->as.Operator.right);
+    }
+    free(e);
+}
 // MAIN :3
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        perror("No File given");
+        fprintf(stderr, "usage: %s <datei>\n", argv[0]);
         return 1;
     }
-    int len;
-    char *parse;
-    struct Token *lex;
-    int numtokens;
 
-    parseFile(argv[1],&parse,&len);
-    printArray(len, parse);
+    char *src = NULL;
+    int   len = 0;
+    if (parseFile(argv[1], &src, &len) != 0) return 1;
 
-    lexer(parse, len, &lex, &numtokens);
+    printArray(len, src);
 
-    printTokens(lex, numtokens);
-    free(parse);
+    struct Token *toks = NULL;
+    int ntoks = 0;
+    lexer(src, len, &toks, &ntoks);
+
+    printTokens(toks, ntoks);
+
+    struct Parser p = { toks, ntoks, 0 };
+    Expr *baum = parseStatement(&p);
+    printExpr(baum, 0);
+
+    freeExpr(baum);
+    free(toks);
+    free(src);
     return 0;
 }
